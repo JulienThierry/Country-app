161638b5acaef9e909233e3dd94c8546d819a954
Julien Thierry <julienthierrypro@gmail.com>
2022-09-06 11:14:49 +0200

    Display countries

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const fetchData = () => {
-  fetch("https://restcountries.com/v3.1/all")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+const countriesList = document.getElementById("countries-list");
+let countries = [];
+
+const displayData = () => {
+  countries.map(
+    (country) =>
+      (countriesList.innerHTML += `
+      <ul>
+          <img src=${country.flags.png} />
+          <li>${country.name.common}</li>
+          <li>${country.capital}</li>
+          <li>Population : ${country.population}</li>
+
+        </ul>
+          `)
+  );
 };
+
+const fetchData = async () => {
+  await fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => (countries = data));
+
+  displayData();
+  console.log(countries);
+};
+
 fetchData();
