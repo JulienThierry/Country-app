6ea040e3fb64cac0970e7bf4e4c4367e8f90e63f
Julien Thierry <julienthierrypro@gmail.com>
2022-09-06 11:43:45 +0200

    Search input

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const countriesList = document.getElementById("countries-list");
+const inputSearch = document.getElementById("inputSearch");
 let countries = [];
 
+inputSearch.addEventListener("input", (e) => {
+  if (e.target.value) {
+    fetchData(e.target.value);
+  } else fetchData("all");
+});
+
 const displayData = () => {
+  countriesList.innerHTML = "";
   countries.map(
     (country) =>
       (countriesList.innerHTML += `
       <ul>
           <img src=${country.flags.png} />
-          <li>${country.name.common}</li>
+          <li>${country.name.official}</li>
           <li>${country.capital}</li>
           <li>Population : ${country.population}</li>
 
@@ -16,13 +24,17 @@ const displayData = () => {
   );
 };
 
-const fetchData = async () => {
-  await fetch("https://restcountries.com/v3.1/all")
-    .then((res) => res.json())
-    .then((data) => (countries = data));
+const fetchData = async (search) => {
+  if (search != "all") {
+    await fetch("https://restcountries.com/v3.1/name/" + search)
+      .then((res) => res.json())
+      .then((data) => (countries = data));
+  } else {
+    await fetch("https://restcountries.com/v3.1/" + search)
+      .then((res) => res.json())
+      .then((data) => (countries = data));
+  }
 
   displayData();
   console.log(countries);
 };
-
-fetchData();
